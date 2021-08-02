6a64803e0298feab1155fa7183cd0648002c8336
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-08-02 18:02:11 -0400

    commit tree 915679eaac71846ffeaa1d7a7b09c51c107c8e21

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,36 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      content: "",
+      charsLeft: props.maxChars
+    };
+  }
+
+  handleTyping = (event) => {
+    const newContent = event.target.value
+    this.setState({
+      content: newContent,
+      charsLeft: (this.props.maxChars - newContent.length)
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+          type="text" 
+          onChange={this.handleTyping}
+          name="message" 
+          id="message" 
+          value={this.state.content} 
+        />
+        <p>Characters Left: {this.state.charsLeft}/{this.props.maxChars}</p>
       </div>
     );
   }
